8cbf0cf49b2f6a11c2984afcc11cff53018a36ef
Ramon Cavalcante <ramoncavpires@gmail.com>
2018-11-15 00:52:17 -0200

    Add de json

--- pizzaria-json/js/script.js
+++ pizzaria-json/js/script.js
@@ -2,9 +2,23 @@ $(document).ready(function(){
 
 
 	function carregaCardapio(){
+		var div = "";
 		$.getJSON('json/pizzas.json', function(data) {
 			$.each(data, function(key, value) {
 				console.log(value.unidades);
+
+				div += "<div class='product'>";
+				div += "<div class='image'>";
+				div += "<img src="+ value.imagem +">";
+				div += "<h3 id='price'>R$ " + value.preco + "</h3>";
+				div += "</div>";
+				div += "<div class='info'>";
+				div += "<h4 id='title'>" + value.nome + "</h4>";
+				div += "<p id='description'>" + value.descricao + "</p>";
+				div += "</div></div>";
+
+
+				$('.content').html(div);
 			});
 		});
     }
