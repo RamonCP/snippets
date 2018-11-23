1750df39c8029c26c3c594ad7545dfa0469ef0b8
Ramon Cavalcante <ramon@techd.com.br>
2018-11-23 17:06:56 -0200

    Add de mais produtos

--- pizzaria-json/js/script.js
+++ pizzaria-json/js/script.js
@@ -1,19 +1,20 @@
-$(document).ready(() => { 
+$(function(){ 
+	carregaCardapio("entradas");
 
+	// Functions
 	function carregaCardapio(arquivo){
 		var div = "";
 		$.getJSON('json/'+ arquivo +'.json', function(data) {
 			$.each(data, function(key, value) {
-				console.log(value.unidades);
-
 				div += "<div class='product'>";
 				div += "<div class='image'>";
 				div += "<img src="+ value.imagem +">";
-				div += "<h3 id='price'>R$ " + value.preco + "</h3>";
+				div += "<h3 id='price' class='mt-4 text-center' data-value="+ value.valor +">R$ " + value.preco + "</h3>";
 				div += "</div>";
 				div += "<div class='info'>";
 				div += "<h4 id='title'>" + value.nome + "</h4>";
 				div += "<p id='description'>" + value.descricao + "</p>";
+				div += "<button type='button' id='add' class='btn btn-warning text-uppercase font-weight-bold py-2 px-5'>Adicionar ao carrinho</button>";
 				div += "</div></div>";
 
 				$('.content').html(div);
@@ -21,11 +22,35 @@ $(document).ready(() => {
 		});
     }
 
-    $('.list ul li a').on("click", () =>{
+    function addCheckout(valor){
+    	// var price_old = parseFloat($('.price a').data('price'));
+    	// $('.price a').attr('data-price',price_old + valor)
+
+    	var total = parseFloat($('.price a').data('price')).trigger('refresh') + valor;
+
+    	// $('.price a').html("R$ "+ total); 
+    	console.log(parseFloat(total))
+
+    }
+
+
+
+
+    // Events
+    $('.list ul li a').click(function(){
     	var link = $(this).data("link");
+    	$('.list ul li a').removeClass('active');
+    	$(this).addClass('active');
     	carregaCardapio(link);
-    })
+    });
 
- 	carregaCardapio("entradas");
+    // on para capturar o evento de um elemento que não foi carregado com o dom
+    $('.content').on("click", "button", function(){
+    	var price = $(this).parents('.info').siblings('.image').find('h3').data('value')
+    	// console.log(price)
+    	addCheckout(price)
+    });
 
+
+ 	
 })
