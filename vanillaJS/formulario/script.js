f2486cd2998c6949727c7d6eba8e0bf79a2b2221
Ramon Cavalcante <ramoncavpires@gmail.com>
2018-12-07 21:21:45 -0200

    Add de alert

--- vanillaJS/formulario/script.js
+++ vanillaJS/formulario/script.js
@@ -7,7 +7,20 @@ document.querySelector('form').addEventListener('submit', function(e){
 	var result = validInput(document.querySelectorAll('input'))
 
 	if (result) {
-		console.log(result)
+		var input_nome = ""
+
+		for (i = 0; i < result.length; i++) {
+			
+			var div = document.querySelector("#"+result[i]).parentElement
+
+			var msg = document.createElement('div')
+			var text = document.createTextNode('Preencha o campo '+ result[i])
+			msg.appendChild(text)
+			msg.classList.add('msg')
+
+			div.appendChild(msg)
+		}
+
 
 	} else {
 		console.log('Todos os inputs preenchidos')
@@ -24,16 +37,15 @@ var validInput = function (elemento) {
 	var input = new Array()
 	for (i = 0; i < elemento.length; i++) {
 		if (elemento[i].value == "") {
-			input.push([elemento[i].value,elemento[i].name])
+			input.push(elemento[i].id)
 			vazio++
 		}
 	}
 
 	if (input.length > 0) {
 		return input 
-
 	} else {
 		return false
 	}
-
 }
+
