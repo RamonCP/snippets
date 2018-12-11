d98503715fce9aa8d77cbfe0833edef1fb6a1a59
Ramon Cavalcante <ramon@techd.com.br>
2018-12-11 10:12:04 -0200

    Diferença entre dias

--- vanillaJS/contador/contador_dias.js
+++ vanillaJS/contador/contador_dias.js
@@ -1,32 +1,32 @@
-var form = document.querySelector('form'),
-	input = document.querySelector('input')
+// var form = document.querySelector('form'),
+// 	input = document.querySelector('input')
 
-form.addEventListener('submit', function(e) {
-	e.preventDefault()
-	if ( input.value != "" ) {
-		console.log(date())
-	} else {
-		alert('input vazio')
-	}
-	console.log('ok')
-})
+// form.addEventListener('submit', function(e) {
+// 	e.preventDefault()
+// 	if ( input.value != "" ) {
+// 		console.log(date())
+// 	} else {
+// 		alert('input vazio')
+// 	}
+// 	console.log('ok')
+// })
 
 function date() {
-	var inicio = document.querySelector('input.inicio'),
-		fim = document.querySelector('input.fim'),
-		data_inicio = new Date(inicio.value),
-		data_fim = new Date(fim.value)
+	var fim = new Date('Jan 01 2019').getTime()
 
-	// data_inicio.setDate(data_inicio.getDate() + 1)
-	// data_fim.setDate(data_fim.getDate() + 1)
+	var hoje = new Date().getTime()
 
-	var day = data_inicio.getDay()
-	var month = data_inicio.getMonth()
-	var year = data_inicio.getFullYear()
+	var diferenca = fim - hoje
 
-	var day_fim = data_fim.getDay()
-	var month_fim = data_fim.getMonth()
-	var year_fim = data_fim.getFullYear()
+	var dias = Math.floor(diferenca / (1000 * 3600 * 24));
+	var horas = Math.floor((diferenca % (1000 * 3600 * 24)) / (1000 * 3600));
+	var segundos = Math.floor((diferenca % (1000 * 60)) / 1000) 
 
-	return day_fim
+
+	document.querySelector('.hours').innerHTML = dias+' dias '+horas+' horas '+segundos+' segundos'
+
 }
+
+setInterval(date, 1000)
+
+// date()
