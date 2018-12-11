d80de272aac0bf13b042095461073b92e4d0c964
Ramon Cavalcante <ramon@techd.com.br>
2018-12-11 12:01:11 -0200

    Exercicio 9

--- vanillaJS/exercicios/09/script.js
+++ vanillaJS/exercicios/09/script.js
@@ -1,3 +1,28 @@
 var btn = document.querySelector('button'),
 	input = document.querySelector('input')
 
+btn.addEventListener('click', function(){
+	if ( input.value != "" ) {
+		alert(pontuacao(input))
+	} else {
+		alert('Preencha o campo')
+	}
+})
+
+function pontuacao(elemento) {
+	var cpf = '';
+	for ( x = 0; x < elemento.value.length; x++ ) {
+
+		if ( x == 2 || x == 5 ) {
+			cpf += elemento.value[x] + '.';
+		} else if ( x == 8 ) {
+			cpf += elemento.value[x] + '-';
+		} else {
+			cpf += elemento.value[x]
+		}
+
+		// cpf += elemento.value[x]		
+	}
+
+	return cpf
+}
