e6313c40a1c7c36ea59848d70a6ff1b396e14441
Ramon Cavalcante <ramon@techd.com.br>
2018-12-12 16:07:16 -0200

    Mask

--- vanillaJS/mask/script.js
+++ vanillaJS/mask/script.js
@@ -1,18 +1,49 @@
 
 
+one('#telefone').addEventListener('keyup', function() {
+	this.value = telefone(this.value)
+})
 
-function telefone(input){
-	var value = keyup(input)
+one('#cpf').addEventListener('keyup', function(){
+	this.value = cpf(this.value)
+})
 
-	console.log(value)
+one('#rg').addEventListener('keyup', function(){
+	this.value = rg(this.value)
+})
+
+one('#cep').addEventListener('keyup', function(){
+	this.value = cep(this.value)
+})
+
+
+function cep(valor) {
+
+}
+
+function rg(valor) {
+	valor = valor.replace(/\D/g,'')
+	valor = valor.replace(/(\d{2})(\d{3})/,"$1.$2.")
+	valor = valor.replace(/(\d{3})(\d{2})/,"$1-$2")
+	return valor
 }
 
 
-function keyup(input) {
-	input.addEventListener('keyup', function(){
-		return this.value	
-	})
+function cpf(valor) {
+	valor = valor.replace(/\D/g,'')
+	valor = valor.replace(/(\d{3})(\d)/,'$1.$2')
+	valor = valor.replace(/(\d{3})(\d{2})$/,'.$1-$2')
+	return valor
 }
+
+
+function telefone(valor) {
+	valor = valor.replace(/\D/g,'')
+	valor = valor.replace(/(\d{2})(\d)/,"($1) $2")
+	valor = valor.replace(/(\d)(\d{4})$/,"$1-$2")
+	return valor
+}
+
 
 function one(elemento) {
 	return document.querySelector(elemento)
@@ -22,7 +53,3 @@ function all(elemento) {
 	return document.querySelectorAll(elemento)
 }
 
-
-
-var phone = one('#telefone')
-telefone(phone)
