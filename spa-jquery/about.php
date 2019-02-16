71c5786546f0587ea86c3ded53862612cd1417b0
Ramon Cavalcante <ramoncavpires@gmail.com>
2019-02-16 20:41:52 -0200

    Add template

---
+++ spa-jquery/about.php
@@ -0,0 +1,28 @@
+	<div id="about">
+			<h1>About Us</h1>
+			<p>Divide have don't man wherein air fourth. Own itself make have night won't make. 
+			   A you under Seed appear which good give. Own give air without fowl moveth dry first 
+               heaven fruit, dominion she'd won't very all.
+           </p>
+           <img src="imagens/signature.png">
+		</div>
+
+		<div id="skills">
+			<div class="professional">
+				<h1>Professional Skills</h1>
+				<div>
+				<p>UI/UX Design 75%</p>
+				<progress value="75" max="100">
+				</div>
+
+				<div>
+				<p>web development 90%</p>
+				<progress value="90" max="100">
+				</div>
+
+				<div>
+				<p>marketing 65%</p>
+				<progress value="65" max="100">
+				</div>
+			</div>
+		</div>
---
+++ spa-jquery/footer.php
@@ -0,0 +1,39 @@
+
+		<footer>
+			<div class="reserved">
+				<h3>Piroll Design, Inc</h3>
+				<p>&copy 2017 Piroll. All rights reserved.<br/>
+				Designed by robirurk.</p>
+			</div>
+
+			<div class="email">
+				<p>hello@pirolltheme.com<br/>
+				+44 987 065 908</p>
+			</div>
+			
+			<div class="link">
+				<ul class="links">
+					<li>Projects</li>
+					<li>About</li>
+					<li>Services</li>
+					<li>Carrer</li>
+				</ul>
+
+				<ul class="links">
+					<li>News</li>
+					<li>Events</li>
+					<li>Contact</li>
+					<li>Legals</li>
+				</ul>
+
+				<ul class="links">
+					<li>Facebook</li>
+					<li>Twitter</li>
+					<li>Instagram</li>
+					<li>Dribbble</li>
+				</ul>
+			</div>
+		</footer>
+	</div>
+</body>
+</html>	
