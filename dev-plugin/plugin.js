1ae616b409a9f2efc4d0b60ea2a76f8373632945
RamonCP <ramoncavpires@gmail.com>
2019-03-24 22:14:48 -0300

    Brincando um pouco mais no plugin

--- dev-plugin/plugin.js
+++ dev-plugin/plugin.js
@@ -1,22 +1,32 @@
 (function($){
 
-    $.fn.meuTooltip = function(options) {
+    $.fn.meuPlugin = function(options) {
 
         // Configuração default
         var defaults = {
-            'corDeFundo':'yellow'
+            'corDeFundo':'yellow',
+            'fontSize': '40px',
+            'text': 'Olá mundo',
+            'statusClass': 'active'
         }
 
         /*  Caso não exista options
             é atribuida para a variável settings
             o valor do defaults
+            $.extend mescla os valores
         */
         var settings =  $.extend({}, defaults, options)
 
         // this é o objeto jQuery
         return this.each(function(){
             //Elemento a ser modificado
-            $(this).css({'background': settings.corDeFundo })
+            $(this)
+            .css({
+                'color': settings.corDeFundo,
+                'font-size': settings.fontSize
+            })
+            .html(settings.text)
+            .addClass(settings.statusClass)
         })
     }
 
--- dev-plugin/script.js
+++ dev-plugin/script.js
@@ -1,5 +1,14 @@
 $(function(){
-    $('.tooltip').meuTooltip({
-        'corDeFundo':'#ff0000'
+    
+    $('p.tooltip').meuPlugin({
+        'corDeFundo':'#ff0000',
+        'text': 'Parágrafo criado a partir do meu plugin',
     });
+
+    $('h1.tooltip').meuPlugin({
+        'corDeFundo': '#007eff',
+        'fontSize': '30px',
+        'text': 'h1 dinâmico vindo do plugin :D',
+        'statusClass':'desativado'
+    })
 })
