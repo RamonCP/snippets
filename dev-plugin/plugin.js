01492aa762eb060ccacef6fec493a867997ff90c
RamonCP <ramoncavpires@gmail.com>
2019-03-24 21:56:27 -0300

    Dev do plugin de tooltip

--- dev-plugin/plugin.js
+++ dev-plugin/plugin.js
@@ -1,15 +1,21 @@
 (function($){
 
-
     $.fn.meuTooltip = function(options) {
 
+        // Configuração default
         var defaults = {
             'corDeFundo':'yellow'
         }
 
+        /*  Caso não exista options
+            é atribuida para a variável settings
+            o valor do defaults
+        */
         var settings =  $.extend({}, defaults, options)
 
+        // this é o objeto jQuery
         return this.each(function(){
+            //Elemento a ser modificado
             $(this).css({'background': settings.corDeFundo })
         })
     }
